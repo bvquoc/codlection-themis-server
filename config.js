79edc9510fc9adace5757515165a1b9615064f23
bvquoc <quocpro10.02.2003@gmail.com>
2021-10-04 18:44:38 +0700

    add config file

---
+++ config.js
@@ -0,0 +1,15 @@
+const firebase = require('firebase');
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAluRe0cXHbkLqSY5Jc6MZ7J1zaFhG6EW8',
+  authDomain: 'ci-onlinejudge.firebaseapp.com',
+  projectId: 'ci-onlinejudge',
+  storageBucket: 'ci-onlinejudge.appspot.com',
+  messagingSenderId: '208800042172',
+  appId: '1:208800042172:web:216326089b914e98f894b9',
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+module.exports = { app, db };
