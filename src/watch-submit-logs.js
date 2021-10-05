bf3122cdce721ba0eb01f881fad9f00f87e4d759
bvquoc <quocpro10.02.2003@gmail.com>
2021-10-05 09:22:09 +0700

    watch log func

--- src/watch-submit-logs.js
+++ src/watch-submit-logs.js
@@ -1,6 +1,5 @@
-const { db, submitDir } = require('../config.js');
+const { db } = require('../config.js');
 const chokidar = require('chokidar');
-const fs = require('fs');
 
 function getArgs(filename) {
   if (!filename || typeof filename !== 'string' || filename.length <= 4 || filename.slice(-3) !== 'log') return;
@@ -16,26 +15,29 @@ function getArgs(filename) {
   };
 }
 
-console.log(`Watching ${submitDir} folder...`);
+function watchLogs(logDir) {
+  const watcher = chokidar.watch(logDir);
+  console.log(`Watching ${logDir} folder...`);
+  watcher.on('change', function (path) {
+    const tmpArr = path.split('/');
+    const filename = tmpArr[tmpArr.length - 1];
+    console.log(tmpArr, filename);
+    if (typeof filename === 'string' && filename.slice(-3) === 'log') {
+      const data = getArgs(filename);
+      console.log('Writing', data);
+      db.collection('logs')
+        .add(data)
+        .then((docRef) => {
+          console.log('Document written with ID: ', docRef.id);
+        })
+        .catch((error) => {
+          console.error('Error adding document: ', error);
+        });
+    }
+  });
+}
 
-var watcher = chokidar.watch(submitDir);
-watcher.on('change', function (path) {
-  const tmpArr = path.split('/');
-  const filename = tmpArr[tmpArr.length - 1];
-  console.log(tmpArr, filename);
-  if (typeof filename === 'string' && filename.slice(-3) === 'log') {
-    const data = getArgs(filename);
-    console.log('Writing', data);
-    db.collection('logs')
-      .add(data)
-      .then((docRef) => {
-        console.log('Document written with ID: ', docRef.id);
-      })
-      .catch((error) => {
-        console.error('Error adding document: ', error);
-      });
-  }
-});
+module.exports = { watchLogs };
 
 // fs.watch('./log', (eventType, filename) => {
 //   db.collection('logs')
