15fc969c9831f4197b68cbe59222907e52508356
bvquoc <quocpro10.02.2003@gmail.com>
2021-10-05 17:20:52 +0700

    add listen-submission func

---
+++ src/listen-submission.js
@@ -0,0 +1,41 @@
+const { db, submitDir } = require('../config.js');
+const fs = require('fs');
+
+const makeFile = (data) => {
+  if (data === void 0) return;
+  console.log('Making', data);
+  const path = `${submitDir}/[${data.userId}][${data.problemId}]${data.submissionId}.${data.language}`;
+  if (fs.existsSync(path)) return;
+
+  fs.writeFile(path, data.code, function (err) {
+    if (err) return console.log(err);
+
+    console.log('The file was saved!');
+  });
+};
+
+subscribeSubmissonsByStatus = (status) => {
+  this.unsubcribesubmissons = db
+    .collection('submissons')
+    .where('status', '==', status)
+    .orderBy('sentAt')
+    .onSnapshot((snapshot) => {
+      snapshot.docChanges().forEach((change) => {
+        if (change.type === 'added') {
+          const curSubmission = {
+            ...change.doc.data(),
+            submissionId: change.doc.id,
+          };
+          makeFile(curSubmission);
+        }
+        if (change.type === 'modified') {
+          // handle on message modified
+        }
+        if (change.type === 'removed') {
+          // handle on remove submissons
+        }
+      });
+    });
+};
+
+subscribeSubmissonsByStatus('pending');
